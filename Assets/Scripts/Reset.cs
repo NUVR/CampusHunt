f866f90ce160682079f6cfa5ea60a9b9bb2c59ee
Discovery Lab <Discovery Lab@ARVR-2.nunet.neu.edu>
2019-11-10 17:56:15 -0500

    Added Try Again

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        SceneManager.LoadScene("One");
+    }
+}
